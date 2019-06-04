b2f712ae5cbddd57268e37e43d3f991834e95966
Cole Holmes <mitchellcholmes@gmail.com>
2019-06-04 02:25:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function getFirstSelector(selector) {
 
 function nestedTarget(number1, number2){
   var result = document.querySelectorALL(number1);
-  var result2= result.querySelector(number2);
+  var result2=result.querySelector(number2);
   return result2;
 }
